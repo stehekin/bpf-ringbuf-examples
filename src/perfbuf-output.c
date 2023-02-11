b52f927b81d122c99cb3f59f2dfc4120b700dd56
Qiang Yu <qiangyu@gmail.com>
2023-02-10 21:20:39 -0800

    map pin

--- src/perfbuf-output.c
+++ src/perfbuf-output.c
@@ -37,24 +37,8 @@ static void sig_handler(int sig)
 	exiting = true;
 }
 
-void handle_event(void *ctx, int cpu, void *data, unsigned int data_sz)
-{
-	const struct event *e = data;
-	struct tm *tm;
-	char ts[32];
-	time_t t;
-
-	time(&t);
-	tm = localtime(&t);
-	strftime(ts, sizeof(ts), "%H:%M:%S", tm);
-
-	printf("%-8s %-5s %-7d %-16s %s\n", ts, "EXEC", e->pid, e->comm, e->filename);
-}
-
 int main(int argc, char **argv)
 {
-	struct perf_buffer *pb = NULL;
-	struct perf_buffer_opts pb_opts = {};
 	struct perfbuf_output_bpf *skel;
 	int err;
 
@@ -75,6 +59,11 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+	err = bpf_map__pin(skel->maps.pb, "/sys/fs/bpf/pb-map");
+	if (err) {
+		fprintf(stderr, "Failed to pin BPF map\n");
+	}
+
 	/* Attach tracepoint */
 	err = perfbuf_output_bpf__attach(skel);
 	if (err) {
@@ -82,34 +71,9 @@ int main(int argc, char **argv)
 		goto cleanup;
 	}
 
-	/* Set up ring buffer polling */
-	pb_opts.sample_cb = handle_event;
-	pb = perf_buffer__new(bpf_map__fd(skel->maps.pb), 8 /* 32KB per CPU */, &pb_opts);
-	if (libbpf_get_error(pb)) {
-		err = -1;
-		fprintf(stderr, "Failed to create perf buffer\n");
-		goto cleanup;
-	}
-
-	/* Process events */
-	printf("%-8s %-5s %-7s %-16s %s\n",
-	       "TIME", "EVENT", "PID", "COMM", "FILENAME");
-	while (!exiting) {
-		err = perf_buffer__poll(pb, 100 /* timeout, ms */);
-		/* Ctrl-C will cause -EINTR */
-		if (err == -EINTR) {
-			err = 0;
-			break;
-		}
-		if (err < 0) {
-			printf("Error polling perf buffer: %d\n", err);
-			break;
-		}
-	}
+	getchar();
 
 cleanup:
-	perf_buffer__free(pb);
 	perfbuf_output_bpf__destroy(skel);
-
 	return err < 0 ? -err : 0;
 }
